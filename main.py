1192c0144009ebda84f40b19ebb49796cd62a9d9
akashcry <93040319+akashcry@users.noreply.github.com>
2024-07-30 23:58:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from flask import Flask, request, jsonify
+import logging
+
 import requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.DEBUG)
 TELEGRAM_BOT_TOKEN = '7354061639:AAHhRpJsx-vIMbF79ujg-f73i8o9epK2Ga0'
-WEBHOOK_URL = f"https://your-vercel-app.vercel.app/{TELEGRAM_BOT_TOKEN}"
-
 @app.route(f'/{TELEGRAM_BOT_TOKEN}', methods=['POST'])
 def telegram_webhook():
     data = request.json
 
+    if data is None:
+        app.logger.error("Received request with no JSON data.")
+        return jsonify({'error': 'No JSON data received'}), 400
+
+    app.logger.debug('Received data: %s', data)
+
     if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        message_text = data["message"]["text"]
+        message = data.get('message', {})
+        chat_id = message.get("chat", {}).get("id")
+        message_text = message.get("text", "")
+
+        app.logger.debug('Chat ID: %s, Message Text: %s', chat_id, message_text)
 
         if message_text == "/start":
-            tg_web_app_data = request.json.get('message').get('web_app_data').get('data')
+            web_app_data = message.get('web_app_data', {})
+            tg_web_app_data = web_app_data.get('data', 'No tg-web-app-data found')
+            app.logger.debug('tgWebAppData: %s', tg_web_app_data)
             send_message(chat_id, f"tgWebAppData: {tg_web_app_data}")
     
     return '', 200
@@ -23,7 +34,8 @@ def telegram_webhook():
 def send_message(chat_id, text):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {"chat_id": chat_id, "text": text}
-    requests.post(url, json=payload)
+    response = requests.post(url, json=payload)
+    app.logger.debug('Send message response: %s', response.json())
 
 if __name__ == "__main__":
     app.run()
